64c03b92e1d2f2012eeea7b47650d087c85e7af2
Emilijus Onusaitis - STUDENT <g00465644@atu.ie>
2026-02-23 11:29:24 +0000

    Initial commit

---
+++ src/main/java/com/example/carpartsservice/CarpartsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.carpartsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpartsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpartsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carpartsservice/CarpartsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carpartsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpartsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
